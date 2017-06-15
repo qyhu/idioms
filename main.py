2568451ad95fc40f4dba0b0494ed596cc49c7edf
qyhu <h_qy@foxmail.com>
2017-06-15 14:55:19 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+def idiom_exists(CY):  #判断成语是否存在
+    with open('.\idiom.txt','r') as f:
+        for i in set(f.readlines()):
+            if CY ==i.strip():
+                return True
+        return False
+
+def idiom_select(x):                               #参数为x的成语，返回该成语的接龙匹配成语
+        with open('.\idiom.txt','r') as f:
+            base = f.readlines()
+            random.shuffle(base)
+            for i in base:
+                if i[:-1] == x or len(i)  != 5:
+                    continue
+                if i[0] == x[-1]:
+                    return i[:-1]
+
+def main():
+    while True:
+        x = input("请输入成语：")
+        if idiom_exists(x):
+            print(idiom_select(x))
+        else:
+            print("该成语不存在，结束")
+            break
+main()
+
+
+
